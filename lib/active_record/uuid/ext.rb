b317d2a68614e8a0c0d6d342bfdba1b03eb87e82
Nando Vieira <fnando.vieira@gmail.com>
2015-05-21 15:05:49 -0300

    Make sure we always override schema statements's create_table method.

--- lib/active_record/uuid/ext.rb
+++ lib/active_record/uuid/ext.rb
@@ -1,6 +1,6 @@
 ActiveSupport.on_load(:active_record) do
   require 'active_record/connection_adapters/postgresql_adapter'
 
-  ActiveRecord::Schema.include ActiveRecord::UUID::Schema
+  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.include ActiveRecord::UUID::Schema
   ActiveRecord::ConnectionAdapters::PostgreSQL::TableDefinition.include ActiveRecord::UUID::TableDefinition
 end
